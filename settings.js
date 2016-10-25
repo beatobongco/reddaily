cfff8cc1c69ae8f800852a719ddcf5e689cdeff9
Beato Bongco <beatobongco@gmail.com>
2016-10-25 09:44:09 +0800

    Add showLimit variable

--- settings.js
+++ settings.js
@@ -1,24 +1,25 @@
 var subredditMax = 3 // how many posts to pull per subreddit from top
+var showLimit = 5 // how many posts to show at first and how many to add for each "read more" click
 var categories = ["tech", "design", "sports", "trivia"]
 var db = {
   tech: {
     subreddits: ["programming", "futurology", "tech", "linux", "learnprogramming", "python", "javascript"],
     posts: [],
-    showLimit: 5
+    showLimit: showLimit
   },
   design: {
     subreddits: ["design", "graphic_design", "web_design", "webdesignnews", "UserExperience", "DataIsBeautiful", "typography"],
     posts: [],
-    showLimit: 5
+    showLimit: showLimit
   },
   sports: {
     subreddits: ["climbing", "bouldering", "climbingvids", "climbharder"],
     posts: [],
-    showLimit: 5
+    showLimit: showLimit
   },
   trivia: {
     subreddits: ["interestingasfuck", "IAmA", "explainlikeimfive", "books", "askhistorians", "LifeProTips", "lifehacks", "YouShouldKnow", "TrueReddit"],
     posts: [],
-    showLimit: 5
+    showLimit: showLimit
   }
 }
